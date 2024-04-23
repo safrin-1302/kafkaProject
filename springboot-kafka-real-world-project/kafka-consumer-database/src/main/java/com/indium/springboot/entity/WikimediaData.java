0961ee9ef03f525ae445aa7bcf0416c53ac5c2ea
safrin-1302 <167766509+safrin-1302@users.noreply.github.com>
2024-04-23 10:16:28 +0530

    Add files via upload

---
+++ springboot-kafka-real-world-project/kafka-consumer-database/src/main/java/com/indium/springboot/entity/WikimediaData.java
@@ -0,0 +1,36 @@
+package com.indium.springboot.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "wikimedia_recentchange")
+@Getter
+@Setter
+public class WikimediaData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String wikiEventData;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getWikiEventData() {
+        return wikiEventData;
+    }
+
+    public void setWikiEventData(String wikiEventData) {
+        this.wikiEventData = wikiEventData;
+    }
+}
---
+++ springboot-kafka-real-world-project/kafka-consumer-database/src/main/java/com/indium/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package com.indium.springboot.repository;
+
+import com.indium.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
